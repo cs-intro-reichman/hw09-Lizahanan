f7f2ed692a870342b4590c16df8b804033ab570d
Elizaveta Khanan <elizaveta.khanan@post.runi.ac.il>
2025-03-11 20:47:19 +0200

    added another method

--- ListIterator.java
+++ ListIterator.java
@@ -23,4 +23,23 @@ public class ListIterator {
         current = current.next;
         return cp;
     }
-}
+
+    //my defined method to get the charData at a given index
+    public CharData getCharData(int index) {
+        // Loop until the desired index
+        for (int i = 0; i < index; i++) {
+            if (current == null) {
+                throw new IndexOutOfBoundsException("Index " + index + " is out of bounds.");
+            }
+            current = current.next;
+        }
+        
+        // Final check if current is null before accessing its data
+        if (current == null) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds.");
+        }
+        
+        return current.cp;
+    }
+    
+    }
